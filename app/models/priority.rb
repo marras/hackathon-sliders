280751bac7e49d1200ffc17e07b5b9cfc1e69afb
Marek Waligórski <marek.waligorski@netguru.pl>
2015-10-16 14:59:11 +0200

    validate range

--- app/models/priority.rb
+++ app/models/priority.rb
@@ -1,10 +1,18 @@
 class Priority
   include Mongoid::Document
   field :name, type: String
-  field :value, type: Float
+  field :value, type: Float, default: 5
 
   belongs_to :project
 
-  validates_presence_of :name
+  validates_presence_of :name, :project, :value
+  validates_uniqueness_of :name, scope: :project_id
+  validate :value_range
+
+  private
+
+  def value_range
+    errors.add(:value, "out of range") unless value.in? (0..10)
+  end
 end
 
